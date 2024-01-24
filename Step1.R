c695c2839baa6887a51a4358253766d0f08256d9
Juan C. Correa <j.correa.n@gmail.com>
2024-01-24 06:35:23 -0600

    Update Step1.R

--- Step1.R
+++ Step1.R
@@ -2,27 +2,42 @@
 # this local folder is a clone of the GitHub Repo
 library(readtext)
 textos <- readtext("Programs/")
-textos$doc_id <- gsub("[^0-9_-]", "", textos$doc_id)
-# The following code allows us  to 
-# classify the type of program (e.g., Master or doctorate)
-# for each program. We need to check the correct 
-# classification 
-library(dplyr)
-textos <- mutate(textos, 
-                 Program = ifelse(
-                   grepl("Maestr", text), "Master",
-                   "Other"))
+Programas <- data.frame(table(textos$doc_id))
+
+textos$doc_id <- sort(as.numeric(gsub("[^0-9]", "", textos$doc_id)), decreasing = FALSE)
+sort(textos$doc_id, decreasing = FALSE)
+textos$University <- c("TEC",
+                       "Insper",
+                       "Pontificia Universidad Católica de Chile",
+                       "Pontificia Universidad Católica de Perú",
+                       "Universdad Adolfo Ibañez Chile",
+                       "Universidad de Los Andes",
+                       "Universidad de Chile",
+                       "Universidad ESAN",
+                       "Universidad de Buenos Aires",
+                       "Universidad ESAN",
+                       "Universidad ORT",
+                       "TEC", 
+                       "Universidad San Ignasio Loyola",
+                       "Universidad ICESI", 
+                       "Universidad del Pacífico", 
+                       "Universidad EAFIT", 
+                       "FIA Business School",
+                       "FIA Business School",
+                       "IESA",
+                       "INCAE")
+
 
 library(quanteda)
 Textos <- corpus(textos$text)
-docvars(Textos, "Program") <- textos$Program
-docvars(Textos, "doc_id") <- textos$doc_id
+docvars(Textos, "Program") <- textos$doc_id
+docvars(Textos, "University") <- textos$University
 
 Programs <- tokens(Textos, 
                      remove_numbers = TRUE, 
                      remove_punct = TRUE, 
                      remove_url = TRUE, 
                      remove_symbols = TRUE) %>%  
-  tokens_remove(stopwords("english"))
+  tokens_remove(stopwords("spanish"))
 
 save.image("Results/Result1.RData")
