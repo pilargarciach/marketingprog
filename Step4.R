4b03e571f9864f7a1ca01a2840e375f66769487a
Juan C. Correa <j.correa.n@gmail.com>
2024-01-25 08:23:16 -0600

    Update Step4.R

--- Step4.R
+++ Step4.R
@@ -15,10 +15,13 @@ Programs <- data.frame(Degree = igraph::degree(BNA),
 Programs <- Programs[ -c(5:25) ]
 rownames(Programs)
 Programs$SS <- rownames(Programs)
-Programs <- Programs[order(Programs$SS), ]
-Programs <- Programs[1:35,]
-Programs <- Programs[1:4]
+Programs <- Programs[!grepl("^text", Programs$SS),]
 colnames(Programs)[4] <- "Eigenvector"
+
+
+
+Programs <- Programs[1:4]
+
 
 library(psych)
 png("F2.png", width = 15, height = 7, units = 'in', res = 300)
@@ -37,37 +40,5 @@ pairs.panels(Programs,
 )
 dev.off()
 
-IM <- as_incidence_matrix(BNA, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNA)$type)
-IM2 <- as.matrix(IM)
-# Let's pick the most important soft skills
-# as per their eigenvector centrality
-selected_columns <- c("S10", "S42", "S44", "S20", "S18", "S3", "S4", "S12", "S6", "S34")
-
-# Subset the matrix by column names
-IM3 <- IM2[, selected_columns, drop = FALSE]
-
-colnames(IM3)[colnames(IM3) == "S10"] <- "Management"
-colnames(IM3)[colnames(IM3) == "S42"] <- "Evaluate"
-colnames(IM3)[colnames(IM3) == "S44"] <- "Decision-Making"
-colnames(IM3)[colnames(IM3) == "S20"] <- "Planning"
-colnames(IM3)[colnames(IM3) == "S18"] <- "Understanding"
-colnames(IM3)[colnames(IM3) == "S3"] <- "Communication"
-colnames(IM3)[colnames(IM3) == "S4"] <- "Creation"
-colnames(IM3)[colnames(IM3) == "S12"] <- "Control"
-colnames(IM3)[colnames(IM3) == "S6"] <- "Leadership"
-colnames(IM3)[colnames(IM3) == "S34"] <- "Change"
-
-library(bipartite)
-png("F3.png", width = 20, height = 7, units = 'in', res = 300)
-plotweb(IM3, method = "normal", 
-        col.high = "orange", 
-        bor.col.high = "orange",
-        col.low = "darkgreen", 
-        bor.col.low = "darkgreen",
-        col.interaction = "grey90",
-        bor.col.interaction = "grey90",
-        low.lablength = 0,
-        labsize = 2)
-dev.off()
 
 save.image("Results/Result4.RData")
