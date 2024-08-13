2085e5e6dc6c3b1fc5c7f25877d794f3660b2837
Juan C. Correa <j.correa.n@gmail.com>
2024-08-12 18:00:28 -0600

    Update ModelsExploration.R

--- ModelsExploration.R
+++ ModelsExploration.R
@@ -21,13 +21,15 @@ model1 <- ergm(red ~ edges + b1sociality(nodes = c(1:28)), control = control.erg
 ))
 summary(model1) # AIC = 6828
 
+model1A <- ergm(red ~ b1sociality(nodes = c(28,27,25)))
+summary(model1A)
+
+rownames(BiM)
+
 model2 <- ergm(red ~ b1sociality(nodes = c(1:28)), control = control.ergm(
   MCMC.burnin = 10000,
   MCMC.interval = 100
 ))
 summary(model2) # AIC = 6826
 
-model3 <- ergm(red ~ gwb1dsp(decay = 2, fixed=TRUE), 
-               control = control.ergm(
-                 MCMC.burnin = 10000,
-                 MCMC.interval = 100))
+
