22c93cdeb6389c1712fb90363203668791ce8547
Juan C. Correa <j.correa.n@gmail.com>
2024-08-14 04:01:33 -0600

    Update Sub-graphsExploration.R

--- Sub-graphsExploration.R
+++ Sub-graphsExploration.R
@@ -9,6 +9,9 @@ network <- Network[!duplicated(Network[c(1,2,4,6,7)]),]
 library(tidyverse)
 Public <- network %>% filter(., grepl("Public",InstitutionType))
 Private <- network %>%  filter(., grepl("Private", InstitutionType))
+AM <- network %>% filter(., grepl("AM", Region))
+EU <- network %>% filter(., grepl("EU-ME-AF", Region))
+AP <- network %>% filter(., grepl("AP", Region))
 
 library(igraph)
 public <- graph_from_data_frame(Public, directed=FALSE)
@@ -20,6 +23,9 @@ PUBLIC <- PUBLIC[ -c(5:25) ]
 PUBLIC$Partition <- "Skills"
 PUBLIC$Partition[28:205] <- "Brochures"
 
+library(psych)
+describeBy(PUBLIC$Degree, group = PUBLIC$Partition, mat = TRUE, digit = 2)
+
 private <- graph_from_data_frame(network,directed=FALSE)
 PRIVATE <- data.frame(Degree = igraph::degree(private),
                  Closeness = igraph::closeness(private),
@@ -29,6 +35,13 @@ PRIVATE <- PRIVATE[ -c(5:25) ]
 PRIVATE$Partition <- "Skills"
 PRIVATE$Partition[29:286] <- "Brochures"
 
+describeBy(PRIVATE$Degree, group = PRIVATE$Partition, mat = TRUE, digit = 2)
 
-boxplot(PUBLIC$Degree ~ PUBLIC$Partition, xlab = "Partition", ylab = "Degree")
-boxplot(PRIVATE$Degree ~ PRIVATE$Partition, xlab = "Partition", ylab = "Degree")
+americas <- graph_from_data_frame(network,directed=FALSE)
+AMERICAS <- data.frame(Degree = igraph::degree(americas),
+                 Closeness = igraph::closeness(americas),
+                 Betweennes = igraph::betweenness(americas),
+                 Eigen = igraph::eigen_centrality(americas))
+AMERICAS <- AMERICAS[ -c(5:25) ]
+AMERICAS$Partition <- "Skills"
+AMERICAS$Partition[29:286] <- "Brochures"
