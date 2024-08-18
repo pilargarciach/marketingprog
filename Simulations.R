7707ebc102f6be820e1e69b53634ecc768ab833b
Juan C. Correa <j.correa.n@gmail.com>
2024-08-17 19:25:12 -0600

    Update Simulations.R

--- Simulations.R
+++ Simulations.R
@@ -1,42 +1,81 @@
 load("Results/Result3.RData")
+rm(list=setdiff(ls(), c("red", "BiM", "BN")))
+SkillsNumber <- data.frame(Skill = rownames(BiM))
 library(ergm)
 library(network)
 library(coda)
 red
 sna::gden(red)
-model0 <- ergm(red ~ edges)
-Model0 <- summary(model0)
-Simuladas0 <- simulate(model0, nsim = 1000)
-Simuladas0[1000]
-class(Simuladas0)
+set.seed(1107)
+ModelA <- ergm(red ~ edges)
+summary(ModelA) # AIC = 8729
+ModelB <- ergm(red ~ b1sociality(nodes = c(3, 11, 13, 2, 6)))
+summary(ModelB) # AIC = 9650 PEOR!
+ModelC <- ergm(red ~ edges + b1sociality(c(3, 11, 13, 2, 6)))
+summary(ModelC) # AIC = 7461
+ModelD <- ergm(red ~ edges + b1sociality(nodes = c(1:28)))
+summary(ModelD) # AIC = 6828
 
-COEF1 <- Model0$coefficients[1]
 
-
-
-
+Simuladas1 <- simulate(ModelC, nsim = 1000)
 library(tidyverse)
-
-# Función para extraer información de una red
-extract_network_info <- function(net) {
-  data.frame(
-    nodos = network.size(net),
-    edges = network.edgecount(net),
-    densidad = network.density(net)
+extract_coefs_simulations <- function(Simuladas1, model) {
+  # Obtener el número de simulaciones
+  num_sims <- length(Simuladas1)
+  
+  # Crear un data frame vacío para almacenar los coeficientes
+  coef_df1 <- data.frame(
+    sim = 1:num_sims,
+    nodos = numeric(num_sims),
+    edges = numeric(num_sims),
+    densidad = numeric(num_sims)
   )
+  
+  # Obtener los nombres de los coeficientes del modelo original
+  coef_names <- names(coef(model))
+  
+  # Iterar sobre cada red simulada y extraer los coeficientes
+  for (i in 1:num_sims) {
+    sim_net <- Simuladas1[[i]]
+    sim_model <- ergm(sim_net ~ edges + b1sociality(c(3, 11, 13, 2, 6)))
+    sim_coefs <- coef(sim_model)
+    
+    # Agregar los coeficientes al data frame
+    coef_df1[i, coef_names] <- sim_coefs
+  }
+  
+  return(coef_df1)
 }
 
-# Aplicar la función a cada red en la lista y crear un dataframe
-df_redes <- map_df(Simuladas0, extract_network_info, .id = "sim_num")
+coef_df1 <- extract_coefs_simulations(Simuladas1, ModelC)
 
-# Convertir la columna sim_num a numérico (si es necesario)
-df_redes$sim_num <- as.numeric(df_redes$sim_num)
+COEF1 <- ModelC$coefficients[2]
+
+
+hist(coef_df1$b1sociality3, main = "Distribución del estadístico en las redes simuladas",
+     xlab = "Valor del estadístico")
+abline(v = COEF1, col = "red", lwd = 2)
+
+
+p_value <- mean(df_redes$edges_ergm >= COEF1)
+
+GOF1 <- gof(ModelC, GOF = ~model)
+GOF1
+
+
+
+
+
+
+
+
 
 
 
 # Modelo con efecto de homofilia en las competencias
-model1 <- ergm(red ~ edges + b1sociality(nodes = c(1:28)))
-summary(model1)
+
+
+
 # Modelo sin intercepto (no recomendado en general)
 model2 <- ergm(red ~ b1sociality(nodes = c(1:28))) 
 
