aec6ee2b0fdd384f79e40a1e2f8008088bd79a35
Juan C. Correa <j.correa.n@gmail.com>
2024-08-19 01:21:01 -0600

    Update Alignment.R

--- Alignment.R
+++ Alignment.R
@@ -4,3 +4,19 @@ Alignment$Skill <- tolower(Alignment$Skill)
 SkillsBN <- BN[1:4]
 SkillsBN$Skill <- rownames(SkillsBN)
 rm(list=setdiff(ls(), c("Alignment", "SkillsBN")))
+alignment <- merge(Alignment, SkillsBN, by = "Skill")
+colnames(alignment)[2] <- "O*NET.Importance"
+library(psych)
+
+pairs.panels(alignment[2:6], 
+             digits = 2, 
+             method = "pearson",
+             cex.cor = 3,
+             stars = TRUE,
+             ci = TRUE,
+             density = TRUE,
+             pch = 8,
+             rug = TRUE,
+             scale = FALSE,
+             cex = 0.5,
+             hist.col = "purple2")
