370460ed53dbee4a53fa1343a10e15c93eeca9f4
Juan C. Correa <j.correa.n@gmail.com>
2024-08-19 00:10:53 -0600

    Update Alignment.R

--- Alignment.R
+++ Alignment.R
@@ -3,3 +3,4 @@ Alignment <- ONET_SkillsImportance[c(2,1)]
 Alignment$Skill <- tolower(Alignment$Skill)
 SkillsBN <- BN[1:4]
 SkillsBN$Skill <- rownames(SkillsBN)
+rm(list=setdiff(ls(), c("Alignment", "SkillsBN")))
