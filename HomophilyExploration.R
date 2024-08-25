a39a262ed9a10c37d504a58b0fc85bc501fdbd2d
Juan C. Correa <j.correa.n@gmail.com>
2024-08-24 20:18:02 -0600

    Update HomophilyExploration.R

--- HomophilyExploration.R
+++ HomophilyExploration.R
@@ -9,8 +9,33 @@
 
 load("Results/Result3.RData")
 rm(list=setdiff(ls(), c("red")))
-red
 library(ergm)
 library(network)
 library(coda)
 red
+
+set.seed(9615)
+Model3h <- ergm(red ~ edges + 
+                  b1sociality(c(3, 11, 13)) + 
+                  b1nodematch("OnetImportance"), 
+                control = control.ergm(MCMC.samplesize = 100000, 
+                                       MCMC.burnin = 10000, 
+                                       MCMLE.maxit = 10))
+summary(Model3h)
+mcmc.diagnostics(Model3h)
+gof(Model3h, GOF =~model)
+
+
+
+
+
+
+Model3H <- ergm(red ~ edges + 
+                   b1sociality(c(3, 11, 13, 2, 6)) + 
+                   b1nodematch("OnetImportance"), 
+                 control = control.ergm(MCMC.samplesize = 100000, 
+                                        MCMC.burnin = 10000, 
+                                        MCMLE.maxit = 10))
+summary(Model3H)
+mcmc.diagnostics(Model3H)
+gof(Model3H, GOF =~model)
