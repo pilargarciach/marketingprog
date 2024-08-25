3bf35eb735c2ce103d1963b12439a7a48af996ac
Juan C. Correa <j.correa.n@gmail.com>
2024-08-24 21:19:22 -0600

    Update HomophilyExploration.R

--- HomophilyExploration.R
+++ HomophilyExploration.R
@@ -26,6 +26,76 @@ mcmc.diagnostics(Model3h)
 gof(Model3h, GOF =~model)
 
 
+Simuladas1 <- simulate(Model3h, nsim = 1000, 
+                       coef = Model3h$coefficients,
+                       control = 
+                         control.simulate.ergm(
+                           MCMC.burnin = 100000, 
+                           MCMC.interval = 500))
+
+
+library(tidyverse)
+extract_coefs_simulations <- function(Simuladas1, model) {
+  # Obtener el número de simulaciones
+  num_sims <- length(Simuladas1)
+  
+  # Crear un data frame vacío para almacenar los coeficientes
+  coef_df1 <- data.frame(
+    sim = 1:num_sims,
+    nodos = numeric(num_sims),
+    edges = numeric(num_sims),
+    densidad = numeric(num_sims)
+  )
+  
+  # Obtener los nombres de los coeficientes del modelo original
+  coef_names <- names(coef(model))
+  
+  # Iterar sobre cada red simulada y extraer los coeficientes
+  for (i in 1:num_sims) {
+    sim_net <- Simuladas1[[i]]
+    sim_model <- ergm(sim_net ~ edges + b1sociality(c(3, 11, 13)) + 
+                        b1nodematch("OnetImportance"))
+    sim_coefs <- coef(sim_model)
+    
+    # Agregar los coeficientes al data frame
+    coef_df1[i, coef_names] <- sim_coefs
+  }
+  
+  return(coef_df1)
+}
+
+coef_df1 <- extract_coefs_simulations(Simuladas1, Model3h)
+
+COEF1 <- Model3h$coefficients[2]
+
+
+hist(coef_df1$b1sociality3, main = "Distribución del estadístico en las redes simuladas",
+     xlab = "Valor del estadístico")
+abline(v = COEF1, col = "red", lwd = 2)
+
+
+t.test(coef_df1$edges, mu = Model3h$coefficients[1])
+t.test(coef_df1$b1sociality3, mu = Model3h$coefficients[2])
+
+cov_sim <- cov(coef_df1[c(3,5:9)])
+observed_stats <- ModelA$coefficients
+coef <- coef_df1[c(3,5:9)]
+
+MD <- mahalanobis(x = t(observed_stats), center = colMeans(coef), cov = cov_sim)
+
+hist(observed_stats, main = "Distribución de las distancias de Mahalanobis", 
+     xlab = "Distancia de Mahalanobis")
+abline(v = MD, col = "red", lwd = 2)
+
+cor(observed_stats, colMeans(coef[1:6]))
+Resumen <- summary(ModelA)
+Resumen$aic
+Resumen$bic
+
+
+mcmc.diagnostics(ModelA) # it works only if force MCMC 
+
+
 
 
 
