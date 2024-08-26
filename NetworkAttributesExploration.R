84d765103423f8f134ab2eea805380884181b84d
Juan C. Correa <j.correa.n@gmail.com>
2024-08-26 12:34:00 -0600

    Update NetworkAttributesExploration.R

--- NetworkAttributesExploration.R
+++ NetworkAttributesExploration.R
@@ -9,4 +9,11 @@ red
 
 Atributos <- data.frame(Nodos = get.vertex.attribute(red, 'vertex.names'),
                         ONET = get.vertex.attribute(red, 'OnetImportance'),
-                        nodos = rownames(BN))
+                        nodos = rownames(BN),
+                        Grado = BN$Degree)
+cor(Atributos$ONET, Atributos$Grado)
+
+library(ggplot2)
+ggplot(Atributos, aes(x=ONET[1:28], y=Grado[1:28])) + 
+  geom_point()+
+  geom_smooth(method=lm)
