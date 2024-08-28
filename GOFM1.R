46765cbf110705829eed7e46f730b4070d18c6d2
Juan C. Correa <j.correa.n@gmail.com>
2024-08-28 16:22:43 -0600

    Update GOFM1.R

--- GOFM1.R
+++ GOFM1.R
@@ -1,5 +1,11 @@
 load("~/Documents/GitHub/marketingprog/Results/ResultsSimM1.RData")
 red
 GOF
-
-
+rm(list = ls())
+load("~/Documents/GitHub/marketingprog/Results/ResultsSimM2.RData")
+GOF
+rm(list = ls())
+load("~/Documents/GitHub/marketingprog/Results/ResultsSimM3.RData")
+GOF
+rm(list = ls())
+load("~/Documents/GitHub/marketingprog/Results/ResultsSimM4.RData")
